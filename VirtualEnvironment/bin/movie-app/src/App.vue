c39f6217ae8d0165cf027a019a33b4089f2b9717
zaraes <zsmi0925@uni.sydney.edu.au>
2021-06-10 01:59:00 +1000

    Complete!

--- VirtualEnvironment/bin/movie-app/src/App.vue
+++ VirtualEnvironment/bin/movie-app/src/App.vue
@@ -1,71 +1,94 @@
 <template>
-  <input v-model="searchRequest" placeholder="search movies" v-on:keyup.enter="searchMovies">
-    <button @click='buttonClick()'>
-        search
-    </button>
-    <p v-text= "searchRequest"></p>
-
-
-  
-  <ul class="list-group">
-    <li class="list-group-item" v-for='movie in movies' :key='movie.id'>
-      <div id="movie">
-      <h4>{{movie.title}} - {{movie.rating}}</h4>
-      {{movie.poster}}
-      <img src="{{movie.poster}}" alt="Poster">
-    </div>
+  <input id = "search-bar" v-model="search" placeholder="search movies" size=50>
     
-    <br>
+    <br><br>
+
+          
+      <span v-for='theater in theaters' :key='theater.name'>
+        <button v-on:click="currTheaterName; setTheatre(theater.name)">{{theater.name}}</button>
+        
+      </span>
+    
+    <br><br>
+  <ul class="list-group">
+    <li class="list-group-item" v-for='movie in filteredMovies' :key='movie.id'>
+      <div id="movie" height=auto width=auto style="overflow:auto">
+        <img v-bind:src = "movie.poster" alt = "Poster" width = 200 align="left">
+        <h4>{{movie.title}}</h4> <h5>{{movie.rating}}</h5>
+        <span id = "times">
+        <li v-for='time in movie.showTimes[currTheaterName]' :key='time'>
+          <div id = "time">{{time}}</div>
+        </li>
+        </span>
+
+        
+        <br><br>
+        
+      </div>
+      <br>
     </li>
   </ul>
   
-  
+
 </template>
 
 <script>
-// fetch('/api/movies').then(res => {
-//   this.movies = res['movies']
-// }) instead of vvvvvvvvvv
 
-//  https://v3.vuejs.org/api/options-lifecycle-hooks.html
-//  https://cli.vuejs.org/guide/deployment.html#github-pages <- building it as a static site to serve on backend ... if you get to it ... no pressure if not.
-
-import movie_metadata from '/Users/zara/Arludo/VirtualEnvironment/bin/movie-app/data/movie_metadata.json'
-import theatre_showtimes from '/Users/zara/Arludo/VirtualEnvironment/bin/movie-app/data/theater_showtimes.json'
+import movie_metadata from './data/movie_metadata.json'
+import theatre_showtimes from './data/theater_showtimes.json'
 
 export default {
   name: 'App',
   components: {},
   data() {
     return {
-      movies: []
+      movies: [],
+      search: '',
+      currTheaterName: theatre_showtimes[0].name
     }
+    
   },
   mounted() {
-    // runs on initialization of the component
-    console.log(movie_metadata)
-    // movie_metadata.forEach(movie => {
-    //   newMovie = {
-    //     ...movie,
-    //     showTimes: {
-    //       "pacificTheatre": []
-    //     }
-    //   }
-    // })
     this.movies = movie_metadata
-    console.log(theatre_showtimes)
+    this.theaters = theatre_showtimes
 
-  //   theatre_showtimes.forEach(theatre => {
-  //     theatre.showTimes.keys().forEach(movieKey => {
-  //       let showTimes = theatre.showTimes[movieKey]
-  //     })
-  //   })
+    
+    this.movies.forEach(currMovie => {
+      currMovie["showTimes"] = {}
+    })
+    console.log(theatre_showtimes)
+    this.searchRequest = ''
+    theatre_showtimes.forEach(theatre => {
+      Object.keys(theatre.showtimes).forEach(movieKey => {
+        let showTimes = theatre.showtimes[movieKey]
+        this.movies.forEach(movObj=>{
+          if(movObj.id == movieKey) {movObj["showTimes"][theatre.name] = showTimes}
+        })
+      })
+    })
   },
-  methods: {
-    buttonClick() {
-      console.log('yaya')
+
+  watch: {
+    search() {
+      this.backdoor++;
+    },
+  },
+  // URL: 
+  computed: {
+    filteredMovies: function() {     
+      return (this.movies.filter((movie) => {
+        return movie.title.toLowerCase().match(this.search.toLowerCase()) && this.currTheaterName in movie.showTimes;
+      }));
+    }
+  },
+
+  methods:{
+    setTheatre: function(theatreName){
+      this.currTheaterName = theatreName
+      console.log("Changing Theatre: "+this.currTheatre)
     }
   }
+
 }
 </script>
 
@@ -77,13 +100,59 @@ export default {
     text-align: center;
   }
   #movie {
-    
-    background-color: lightblue;
+    background-color: white;
     text-align: center;
     max-width: 500px;
     width: 50%;
     margin: auto;
     padding: 10px;
+    -moz-box-shadow:   0 0 10px #A9ABAB;
+   -webkit-box-shadow: 0 0 10px #A9ABAB;
+   box-shadow:         0 0 10px #A9ABAB;
+   border-radius: 25px;
+  }
+  h4 {
+
+  }
+  h5 {
+
+    color: #878787;
+  }
+
+  img{
+    text-align: left;
+    padding-left: 5px;
+    padding-top: 5px;
+    padding-bottom: 5px;
+    
+  }
+  body {
+    background-color: #DADFE3
+  }
+
+  #search-bar {
+    border-radius: 10px;
+    border-color: #DADFE3;
+  }
+
+  button {
+    background-color: #DADFE3;
+    border-radius: 25px;
+    border-style: none;
+  }
+
+  #times{
+    display:inline-block;
+
+  }
+
+  #time {
+    display:inline;
+    text-align: center;
+  }
+
+  p {
+    font-size: 5px;
   }
   
 </style>
